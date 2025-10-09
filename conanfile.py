2ec55fff869de743e819cbad8ea601176d8b0d92
Thomas Kroes <tkroes81@gmail.com>
2025-10-09 10:53:47 +0200

    Implement PDB file copying for Windows builds
    Added functionality to copy PDB files for Windows builds to the Conan package.

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,8 @@ import shutil
 import pathlib
 import subprocess
 from rules_support import PluginBranchInfo
-
+from conans import tools
+import shutil
 
 class ClusterDifferentialExpressionPluginConan(ConanFile):
     """Class to package DifferentialExpressionPlugin using conan
@@ -156,6 +157,17 @@ class ClusterDifferentialExpressionPluginConan(ConanFile):
                 release_dir,
             ]
         )
+
+        # Add the PDB files to the Conan package
+        if self.settings.os == "Windows":
+            print("Copying PDBs...")
+            pdb_dest = pathlib.Path(package_dir, "RelWithDebInfo", "PDBs")
+            pdb_dest.mkdir()
+            pdb_files = pdb_files = [p for p in pathlib.Path(self.build_folder).rglob('*') if p.is_file() and p.suffix.lower() == '.pdb']
+            print("PDB(s): ", pdb_files)
+            for pfile in pdb_files:
+                shutil.copy(pfile, pdb_dest)
+
         self.copy(pattern="*", src=package_dir)
 
     def package_info(self):
